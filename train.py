b25da8299387baa4ac8c781675e6260620fdb30c
ksh1040 <ksh1040@kaist.ac.kr>
2022-08-25 13:47:52 +0900

    Neptune AI logger implementation

--- train.py
+++ train.py
@@ -8,11 +8,36 @@ from __future__ import absolute_import, division, print_function
 
 from trainer import Trainer
 from options import MonodepthOptions
+import neptune.new as neptune
 
 options = MonodepthOptions()
 opts = options.parse()
 
 
 if __name__ == "__main__":
+    run = neptune.init(
+        project="ksh/MonoSVO",
+        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkMmI1MDI3Ny0zZDIwLTRhOGMtOTQ2OS1kOGJiMTVlNDNjM2IifQ==",
+        source_files=[
+            "trainer.py",
+            "networks/[A-Za-z]*.py",
+            "datasets/[A-Za-z]*.py"
+        ],
+        tags=["monodepth2"]
+    )
+
+    params = {
+        "model_name": opts.model_name,
+        "split": opts.split,
+        "dataset": opts.dataset,
+        "learning_rate": opts.learning_rate,
+        "scheduler_step_size": opts.scheduler_step_size,
+        "batch_size": opts.batch_size,
+        "num_epochs": opts.num_epochs,
+        "pose_model_type": opts.pose_model_type,
+        "deformable_conv": opts.deformable_conv,
+        "uncertainty_input": opts.uncertainty_input}
+    run["parameters"] = params
+
     trainer = Trainer(opts)
     trainer.train()
